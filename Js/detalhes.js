ace2119b65145135632d8e43ba90ac19cb065417
Arthur Augusto Zica de Almeida <arthuraugustozica@gmail.com>
2022-12-01 18:20:34 -0300

    mudado fundao da img

--- Js/detalhes.js
+++ Js/detalhes.js
@@ -15,17 +15,16 @@ function getGameById(id){
         $('#destaque-data').html("Data de lançamento: "+corpo.released);
 
         //use the game image as background at css file
+        document.getElementById("corpoSite").style.backgroundColor = 'rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 0.8+')';
+        document.getElementById("corpoSite").style.backgroundBlendMode = "multiply";
         document.getElementById("corpoSite").style.backgroundImage = "url("+corpo.background_image+")";
         document.getElementById("corpoSite").style.backgroundSize = "cover";
         document.getElementById("corpoSite").style.backgroundRepeat = "no-repeat";
         document.getElementById("corpoSite").style.backgroundPosition = "center";
         document.getElementById("corpoSite").style.backgroundAttachment = "fixed";
         document.getElementById("corpoSite").style.backgroundSize = "100% 100%";
+        
 
         
     })
 }
-function setTopo(){
-    $(window).scrollTop(0);
-}
-$(window).bind('scroll', setTopo);
