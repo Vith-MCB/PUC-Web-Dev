ff3b3797e85a1d0164b51eccc714a6365adde472
Vith-MCB <vitorriblacerda@gmail.com>
2022-12-01 20:48:43 -0300

    last change

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-if (window.location.href.includes("index.html")) {
+if (window.location.href.includes("GamePortal")) {
 
     var contagem = 1;
     fetch('https://api.rawg.io/api/games?key=84860f89c4ba45e0ab92ab9c686dd12c').then(resposta=>{return resposta.json()})
